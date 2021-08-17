836dec6853d44997ebde0919c41d3778a71b658e
PartMan <mane.parth@gmail.com>
2021-08-17 19:25:37 +0530

    Remove config test

--- test/config.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const config = require('../src/config.js');
-
-describe('Config', () => {
-	it('should have DEBUG set to false', () => {
-		assert.equal(config.DEBUG, false);
-	});
-});
