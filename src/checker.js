494d3f39fbaadb1e647a5ff686eca923a5517d17
PartMan <mane.parth@gmail.com>
2021-08-28 00:54:12 +0530

    Public release let's go

--- src/checker.js
+++ src/checker.js
@@ -1,8 +1,8 @@
 exports.compare = function compare (puzzle_type, date, obj) {
 	const tools = require('./tools.js');
 	const solutions = require('./solutions.json');
-	if (!solutions.hasOwnProperty(puzzle_type)) return -1;
-	const puzzles = solutions[puzzle_type];
-	if (!puzzles.hasOwnProperty(date)) return -1;
-	return tools.deepEquals(puzzles[date], obj);
+	if (!solutions.hasOwnProperty(date)) return -1;
+	const puzzles = solutions[date];
+	if (!puzzles.hasOwnProperty(puzzle_type)) return -1;
+	return tools.deepEquals(puzzles[puzzle_type], obj);
 };
