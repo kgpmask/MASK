29ab71091219bb7cc8fcb9aa481493d90aec1fcf
PartMan <mane.parth@gmail.com>
2022-03-06 13:30:45 +0530

    ;

--- src/handler.js
+++ src/handler.js
@@ -19,7 +19,7 @@ function xmur3 (str) {
 		h = Math.imul(h ^ (h >>> 16), 2246822507);
 		h = Math.imul(h ^ (h >>> 13), 3266489909);
 		return (h ^= h >>> 16) >>> 0;
-	}
+	};
 }
 
 function fakeRandom (seed) {
@@ -29,7 +29,7 @@ function fakeRandom (seed) {
 		t = Math.imul(t ^ t >>> 15, t | 1);
 		t ^= t + Math.imul(t ^ t >>> 7, t | 61);
 		return ((t ^ t >>> 14) >>> 0) / 4294967296;
-	}
+	};
 }
 
 function handler (app, env, vapid) {
