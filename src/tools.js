48845e39fcf9ec581b55422fcd75eb2a419f9fcf
PartMan <mane.parth@gmail.com>
2022-07-08 20:11:44 +0530

    Remove comment / arrowizing

--- src/tools.js
+++ src/tools.js
@@ -43,7 +43,7 @@ function xmur3 (str) {
 	for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
 		h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
 		h = h << 13 | h >>> 19;
-	} return function() {
+	} return () => {
 		h = Math.imul(h ^ (h >>> 16), 2246822507);
 		h = Math.imul(h ^ (h >>> 13), 3266489909);
 		return (h ^= h >>> 16) >>> 0;
@@ -52,7 +52,7 @@ function xmur3 (str) {
 
 exports.fakeRandom = function fakeRandom (seed) {
 	let a = xmur3(seed)();
-	return function () {
+	return () => {
 		let t = a += 0x6D2B79F5;
 		t = Math.imul(t ^ t >>> 15, t | 1);
 		t ^= t + Math.imul(t ^ t >>> 7, t | 61);
