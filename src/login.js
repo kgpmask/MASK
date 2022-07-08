267ac6fb7b2ad76869955c0c1e9160a7796201eb
PartMan <mane.parth@gmail.com>
2022-07-08 20:50:17 +0530

    Line length

--- src/login.js
+++ src/login.js
@@ -1,10 +1,13 @@
 const GoogleStrategy = require('passport-google-oauth20');
 const dbh = require('../database/database_handler');
 
+const port = require('./config.js').PORT;
+const callbackURL = (process.env['NODE_ENV'] === 'production' ? 'https://mask-kgp.club' : `http://localhost:${port}`);
+
 passport.use(new GoogleStrategy({
 	clientID: process.env['GOOGLE_CLIENT_ID'],
 	clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-	callbackURL: (process.env['NODE_ENV'] === 'production' ? 'https://mask-kgp.club' : `http://localhost:${require('./config.js').PORT}`) + '/oauth2/redirect/google',
+	callbackURL: callbackURL + '/oauth2/redirect/google',
 	scope: ['profile'],
 	state: true
 }, (_, __, profile, cb) => dbh.createNewUser(profile).then(res => cb(null, res)).catch(err => cb(err))));
