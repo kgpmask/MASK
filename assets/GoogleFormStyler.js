646dcc3f834afddf03ced6d37e5042d2976b9e9c
PartMan <mane.parth@gmail.com>
2022-08-04 22:56:11 +0530

    Update icons
    Change icon naming scheme

---
+++ assets/GoogleFormStyler.js
@@ -0,0 +1,22 @@
+(function (XHR) {
+    var open = XHR.prototype.open;
+    XHR.prototype.open = function (method, url, async, user, pass) {
+        this._url = url;
+        if (url.indexOf("gstatic.com") !== -1 ||
+            url.indexOf("docs.google.com") !== -1) {
+            url = "http://googleformrestyler.apixml.net/corsProxy.aspx?base64Url=" + btoa(url);
+        }
+        open.call(this, method, url, async, user, pass);
+    };
+})(XMLHttpRequest);
+(function() {
+    var script = document.currentScript ||
+        /*Polyfill*/ Array.prototype.slice.call(document.getElementsByTagName('script')).pop();
+    var URL = script.getAttribute('form');
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", URL);
+    xhr.onload = function() {
+        document.write(xhr.response);
+    };
+    xhr.send();
+})();
