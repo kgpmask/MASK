dd00dc6d7659ef2c8d6a5de4663f558c0381ff83
Vidunram A R <64352864+Goose-Of-War@users.noreply.github.com>
2022-09-14 22:55:31 +0530

    Straight up rejection ;-;
    No throwing string rejects. Just rejections. I hope karma doesn't strike me with rejections ;-;

--- src/checker.js
+++ src/checker.js
@@ -1,9 +1,9 @@
 exports.compare = function compare (puzzleType, date, obj) {
 	return new Promise((resolve, reject) => {
 		const solutions = require('./solutions.json');
-		if (!solutions.hasOwnProperty(date)) throw reject("No date in the puzzle.");
+		if (!solutions.hasOwnProperty(date)) reject(new Error("No date in the puzzle."));
 		const puzzles = solutions[date];
-		if (!puzzles.hasOwnProperty(puzzleType)) throw reject("Puzzle without a type... sus");
+		if (!puzzles.hasOwnProperty(puzzleType)) reject(new Error("Puzzle without a type... sus"));
 		if (puzzleType.startsWith('quiz')) return resolve(puzzles[puzzleType][obj.index]);
 		resolve(Tools.deepEquals(puzzles[puzzleType], obj));
 	});
