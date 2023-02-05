bed981d15d43963d28d4be5b8c72e30696d87713
PartMan <mane.parth@gmail.com>
2023-02-06 01:41:09 +0530

    Added rudimentary testing

--- test/site.js
+++ test/site.js
@@ -1,21 +1,19 @@
 const assert = require('assert');
 const axios = require('axios');
 const server = require('../src/mask.js');
-const config = require('../src/config.js');
+const { PORT } = require('../src/config.js');
 
 const pages = ['', 'home', 'art', 'videos', 'events', 'about', 'members', 'submissions', '404'];
 
 before(() => server.ready());
 
-console.log(">>>>>SECRET>>>>>", process.env.SESSION_SECRET); // Well this isn't particularly dangerous...
-
-describe('server', () => {
+describe('Server', () => {
 	pages.forEach(page => {
-		it(`should serve page (${page || '/'})`, () => axios.get(`http://localhost:${config.PORT}/${page}`)).timeout(1000);
+		it(`should serve page (${page || '/'})`, () => axios.get(`http://localhost:${PORT}/${page}`)).timeout(1_000);
 		// Pages should render in under a second
 	});
 
-	it('should display 404s for pages that don\'t exist', () => axios.get(`http://localhost:${config.PORT}/hashire-sori-yo`)
+	it('should display 404s for pages that don\'t exist', () => axios.get(`http://localhost:${PORT}/hashire-sori-yo`)
 		.then(() => Promise.resolve(false))
 		.catch(res => assert.equal(res.response.status, 404)));
 });
