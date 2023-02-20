1e623decd1276df419752078a8583d14dcc7a92f
PartMan <mane.parth@gmail.com>
2023-02-20 21:38:29 +0530

    bash stuff

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,11 +1,17 @@
 if [ "$1" == "dev" ] ; then
-	command docker build -t mask_dev_image -f ./docker/Dockerfile-dev . &&
-	command docker container rm mask_dev &&
+	if ! command docker build -t mask_dev_image -f ./docker/Dockerfile-dev .
+	then
+		exit 1
+	fi
+	command docker container rm mask_dev
 	command docker run --name mask_dev -dp 6971:6969 mask_dev_image &&
 	echo "Dev docker is running!"
 elif [ "$1" == "prod" ] ; then
-	command docker build -t mask_image -f ./docker/Dockerfile . &&
-	command docker container rm mask &&
+	if ! command docker build -t mask_image -f ./docker/Dockerfile .
+	then
+		exit 1
+	fi
+	command docker container rm mask
 	command docker run --name mask -dp 6971:6969 mask_image &&
 	command echo "Dev docker is running!"
 else
