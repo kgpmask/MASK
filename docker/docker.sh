5fd4c31008bbe316c21741b402909cf324b6dc26
PartMan <mane.parth@gmail.com>
2023-02-20 21:31:21 +0530

    Docker better names

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,14 +1,14 @@
 set -e
 
 if [ "$1" == "dev" ] ; then
-	command docker build -t mask_dev -f ./docker/Dockerfile-dev .
+	command docker build -t mask_dev_image -f ./docker/Dockerfile-dev .
 	command docker container rm mask_dev
-	command docker run --name mask_dev -dp 6971:6969 mask_dev
+	command docker run --name mask_dev -dp 6971:6969 mask_dev_image
 	command echo "Dev docker is running!"
 elif [ "$1" == "prod" ] ; then
-	command docker build -t mask -f ./docker/Dockerfile .
+	command docker build -t mask_image -f ./docker/Dockerfile .
 	command docker container rm mask
-	command docker run --name mask -dp 6971:6969 mask
+	command docker run --name mask -dp 6971:6969 mask_image
 	command echo "Dev docker is running!"
 else
 	command echo "No environment specified."
