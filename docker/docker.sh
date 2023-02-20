2758606728a4af2d2767ae6c75d1df042020b495
PartMan <mane.parth@gmail.com>
2023-02-20 21:27:25 +0530

    ...forgot to fix the name

--- docker/docker.sh
+++ docker/docker.sh
@@ -3,7 +3,7 @@ set -e
 if [ "$1" == "dev" ] ; then
 	command docker build -t mask-dev -f ./docker/Dockerfile-dev .
 	command docker container rm mask_dev
-	command docker run --name mask_dev -dp 6971:6969 mask-dev
+	command docker run --name mask_dev -dp 6971:6969 mask_dev
 	command echo "Dev docker is running!"
 elif [ "$1" == "prod" ] ; then
 	command docker build -t mask -f ./docker/Dockerfile .
