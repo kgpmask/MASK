b333c7ec80a6c75a151e950ca0169b4f35aed40f
PartMan <mane.parth@gmail.com>
2023-02-20 21:29:35 +0530

    So I am very dumb

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,7 @@
 set -e
 
 if [ "$1" == "dev" ] ; then
-	command docker build -t mask-dev -f ./docker/Dockerfile-dev .
+	command docker build -t mask_dev -f ./docker/Dockerfile-dev .
 	command docker container rm mask_dev
 	command docker run --name mask_dev -dp 6971:6969 mask_dev
 	command echo "Dev docker is running!"
@@ -10,4 +10,6 @@ elif [ "$1" == "prod" ] ; then
 	command docker container rm mask
 	command docker run --name mask -dp 6971:6969 mask
 	command echo "Dev docker is running!"
+else
+	command echo "No environment specified."
 fi
