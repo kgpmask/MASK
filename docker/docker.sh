2067bfc319efa06b8822102370f54138f7518c7a
PartMan <mane.parth@gmail.com>
2023-02-20 20:40:20 +0530

    No RM? :sad:

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,10 +2,10 @@ set -e
 
 if [ "$1" == "dev" ] ; then
 	command docker build -t mask-dev -f ./docker/Dockerfile-dev .
-	command docker run --name mask_dev -rm -p 6971:6969 mask-dev
+	command docker run --name mask_dev -p 6971:6969 mask-dev
 	command echo "Dev docker is running!"
 elif [ "$1" == "prod" ] ; then
 	command docker build -t mask -f ./docker/Dockerfile .
-	command docker run --name mask -rm -p 6971:6969 mask
+	command docker run --name mask -p 6971:6969 mask
 	command echo "Dev docker is running!"
 fi
