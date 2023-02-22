5b8088651fe83a2ee8d1662c0213ae019df46949
Ankan <ankansahadgp@gmail.com>
2023-02-22 12:19:59 +0530

    handler.js

--- src/handler.js
+++ src/handler.js
@@ -113,45 +113,53 @@ function handler (app, nunjEnv) {
 		return res.renderFile('about.njk');
 	});
 	app.get('/members/:yearName?', (req, res) => {
-		const membersData = require('./members.json');
-		const yearName = req.params.yearName || membersData[0].name;
-		const yearIndex = membersData.findIndex(year => [year.name, year.baseYear].includes(yearName));
-		if (yearIndex === -1) return res.notFound();
-		const {
-			name,
-			baseYear,
-			teams,
-			members
-		} = membersData[yearIndex];
-		const ctx = { 'Governors': [], 'Former Members': [], 'Research Associate': [] };
-		members.sort((a, b) => -(a.name < b.name)).forEach(member => {
-			let target;
-			if (member.gov) target = 'Governors';
-			else if (member.inactive) target = 'Former Members';
-			else if (member.RA) target = 'Research Associate';
-			else target = `Batch of 20${member.roll.substr(0, 2)}`;
-			if (!ctx[target]) ctx[target] = [];
-			ctx[target].push({
-				name: member.name,
-				roll: member.roll,
-				href: member.id.startsWith('X') ? 'blank.webp' : `${member.id}.webp`,
-				teams: member.teams.map(teamID => {
-					const team = teams[teamID.toLowerCase()];
-					if (teamID === teamID.toUpperCase()) return { name: team.name, icon: team.icon + '-head' };
-					return team;
-				})
+		const membersData = require('./newMembers.json');
+		const yearName = 2022;
+		const teamsData = require("./teams.json");
+		const yearMembers = [];
+		membersData.forEach(member => {
+			if (member.records.forEach(rec => rec.year === yearName)) {
+				const mem = {
+					image: member.image,
+					name: member.name,
+					roll: member.roll,
+					position: rec.position,
+					teams: rec.teams
+				};
+				yearMembers.push(mem);
+			}
+		});
+		const status = {
+			'Governor': [],
+			'Former Member': [],
+			'Research Associate': [],
+			'Advisor': [],
+			'Executive': [],
+			'Associate': [],
+			'Fresher': []
+		};
+		membersData.sort((a, b) => -(a.name < b.name)).forEach(mem => {
+			mem.records.forEach(yearData => {
+				if (yearData.year === yearName) {
+					status[yearData.position].push({
+						name: mem.name,
+						roll: mem.roll,
+						image: mem.image,
+						teams: yearData.teams.map(teamID => {
+							const team = {
+								name: teamsData[yearName][teamID[0]].name,
+								icon: teamsData[yearName][teamID[0]].icon
+							};
+							team.icon += teamID[1] === "H" ? "-head" : teamID[1] === "S" ? "-sub" : '';
+							return team;
+						})
+					});
+				}
 			});
 		});
-		const prev = membersData[yearIndex + 1]?.name, next = membersData[yearIndex - 1]?.name;
-		const keys = [
-			'Governors',
-			'Research Associate',
-			...Object.keys(ctx).filter(key => key.startsWith('Batch of ')).sort(),
-			'Former Members'
-		];
-		const membersObj = Object.fromEntries(keys.map(key => [key, ctx[key]]));
-		const membersTitle = name === membersData[0].name ? 'Our Members' : name;
-		return res.renderFile('members.njk', { members: membersObj, membersTitle, prev, next });
+		const membersObj = Object.entries(status);
+		const membersTitle = 'Our Members';
+		return res.renderFile('members.njk', { membersObj: membersObj, prev: 2021, membersTitle: membersTitle, next: undefined });
 	});
 
 
