b8a72144f52eb12052ae212277d33ffe33e07d6a
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-02-22 09:02:45 +0530

    Commenting...

--- database/schemas/Member.js
+++ database/schemas/Member.js
@@ -7,7 +7,9 @@ const memberSchema = new mongoose.Schema({
 	imageLink: String,
 	records: [{
 		year: { type: Number, required: true },
+		// Positions: Governor, Research Associate, Executive, Associate, Fresher
 		position: String,
+		// Team: x = member, xH = x Head, xS = x Sub-Head
 		teams: { type: [String], required: true }
 	}]
 }, { collection: 'member' });
