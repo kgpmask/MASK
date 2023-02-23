7aed967541308648a2e27b070e87199210457198
Ankan <ankansahadgp@gmail.com>
2023-02-23 11:10:19 +0530

    mongoless mode

--- src/handler.js
+++ src/handler.js
@@ -113,8 +113,134 @@ function handler (app, nunjEnv) {
 		return res.renderFile('about.njk');
 	});
 	app.get('/members/:yearName?', async (req, res) => {
+		const   sample = [
+			{
+				name: 'Ankan Saha',
+				roll: '22EE10008',
+				image: '../assets/members/22_ankan.webp',
+				teams: [
+					{
+						name: 'Quiz',
+						icon: 'quiz'
+					},
+					{
+						name: 'WebDev',
+						icon: 'webdev'
+					}
+				],
+				position: 'Fresher'
+			},
+			{
+				name: 'Jai Sachdev',
+				roll: '22BT10011',
+				image: '../assets/members/22_jai.webp',
+				teams: [
+					{
+						name: 'WebDev',
+						icon: 'webdev'
+					}
+				],
+				position: 'Fresher'
+			},
+			{
+				name: 'Karthikeya S M Yelisetty',
+				roll: '21CS30060',
+				image: '../assets/members/21_karthikeya.webp',
+				teams: [
+					{
+						name: 'AMV & Music',
+						icon: 'amv'
+					},
+					{
+						name: 'WebDev',
+						icon: 'webdev'
+					}
+				],
+				position: 'Former Member'
+			},
+			{
+				name: 'Parth Mane',
+				roll: '19MF10022',
+				image: '../assets/members/19_parth.webp',
+				teams: [
+					{
+						name: 'AMV & Music',
+						icon: 'amv'
+					},
+					{
+						name: 'Media & Newsletter',
+						icon: 'newsletter'
+					},
+					{
+						name: 'Quiz',
+						icon: 'quiz'
+					},
+					{
+						name: 'WebDev',
+						icon: 'webdev-head'
+					}
+				],
+				position: 'Team Heads'
+			},
+			{
+				name: 'Saumyadip Nandy',
+				roll: '20EC10072',
+				image: '../assets/members/20_saumyadip.webp',
+				teams: [
+					{
+						name: 'WebDev',
+						icon: 'webdev'
+					}
+				],
+				position: 'Former Member'
+			},
+			{
+				name: 'Uday Srivastava',
+				roll: '22ME30072',
+				image: '../assets/members/22_uday.webp',
+				teams: [
+					{
+						name: 'WebDev',
+						icon: 'webdev'
+					}
+				],
+				position: 'Fresher'
+			},
+			{
+				name: 'Venkatsai Mokshith',
+				roll: '21CS10050',
+				image: '../assets/members/21_venkatsai.webp',
+				teams: [
+					{
+						name: 'WebDev',
+						icon: 'webdev'
+					}
+				],
+				position: 'Associate'
+			},
+			{
+				name: 'Vidunram A R',
+				roll: '21EE30033',
+				image: '../assets/members/21_vidunram.webp',
+				teams: [
+					{
+						name: 'Media & Newsletter',
+						icon: 'newsletter'
+					},
+					{
+						name: 'Quiz',
+						icon: 'quiz'
+					},
+					{
+						name: 'WebDev',
+						icon: 'webdev-sub'
+					}
+				],
+				position: 'Team Sub-Heads'
+			}
+		];
 		const yearName = parseInt(req.params.yearName) || 2022;
-		const membersData = await dbh.getMembersbyYear(yearName);
+		const membersData = PARAMS.mongoless ? sample : await dbh.getMembersbyYear(yearName);
 		const status = {
 			'Governors': [],
 			'Team Heads': [],
