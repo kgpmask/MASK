7877f5c6d10ab1aad42d3fd02016b6f1a814aca1
PartMan <mane.parth@gmail.com>
2023-02-27 14:34:47 +0530

    express gang

--- routes/live.js
+++ routes/live.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 const checker = require('../src/checker.js');
--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const fs = require('fs').promises;
 const path = require('path');
