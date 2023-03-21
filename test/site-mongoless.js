838e2a3ace6105b41a7257312da86d575d3481fd
Ankan <ankansahadgp@gmail.com>
2023-03-21 20:26:42 +0530

    looks good, and it works ig, teach me senpai :heh:

---
+++ test/site-mongoless.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const axios = require('axios');
+const server = require('../src/mask.js');
+const PORT = 42069;
+
+const pages = ['', 'home', 'art', 'videos', 'about', 'members', 'submissions'];
+const oldPARAMS = PARAMS;
+PARAMS.mongoless = true;
+
+before(() => server.ready());
+describe('Server (Mongoless mode)', () => {
+	pages.forEach(page => {
+		it(`should serve page (${page || '/'})`, () => axios.get(`http://localhost:${PORT}/${page}`))
+			.timeout(process.platform === 'win32' ? 5_000 : 1_500);
+		// Pages should render in under 1.5s (or 5 seconds on Windows)
+	});
+
+	it('should display 404s for pages that don\'t exist', () => axios.get(`http://localhost:${PORT}/hashire-sori-yo`)
+		.then(() => Promise.resolve(false))
+		.catch(res => assert.equal(res.response.status, 404)));
+});
+
+PARAMS = oldPARAMS;
+after(() => server.close());
