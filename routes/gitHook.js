1c6d69fd4b3a2291fb1b2085a1e4fffcf153f9f5
Ankan <ankansahadgp@gmail.com>
2023-04-20 21:14:52 +0530

    git hook thing

--- routes/gitHook.js
+++ routes/gitHook.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
 	}
 	const branch = process.env.WEBHOOK_BRANCH;
 	if (!branch) return res.send('No branch configured for webhooks');
-	if (branch !== 'dev') return res.send('Automatic webhook updates are only enabled on the dev branch');
+	if (branch !== 'dev' || branch !== 'main') return res.send('Automatic webhook updates are only enabled on dev and main branch');
 	await Tools.updateCode();
 	res.send('Success!');
 	return process.exit(0);
