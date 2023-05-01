9d9d8c3d628a92d8610db8d8e2c1a6c8178e80fe
Ankan <ankansahadgp@gmail.com>
2023-05-01 23:40:58 +0530

    this is jai from ankan's laptop yes

--- src/handler.js
+++ src/handler.js
@@ -729,19 +729,26 @@ function handler (app, nunjEnv) {
 		return process.exit(0);
 	});
 
-	app.post('/', (req, res) => {
+	app.use((req, res, next) => {
 		// If propagation hasn't stopped, switch to GET!
-		return res.redirect(req.url);
+		if (req.method === "POST") {
+			return res.redirect(req.url);
+		}
+		next();
 	});
-	app.get('/', (req, res) => {
+	app.use((req, res, next) => {
 		// Catch-all 404
 		res.notFound();
+		next();
 	});
 
 	app.use((err, req, res, next) => {
+		if (!err.stack) {
+			next(err);
+		}
 		if (PARAMS.dev) console.error(err.stack);
 		// Make POST errors show only the data, and GET errors show the page with the error message
-		res.status(500).renderFile('404.njk', { message: 'Server error! This may or may not be due to invalid input.' });
+		res.status(500).render('404.njk', { message: 'Server error! This may or may not be due to invalid input.' });
 	});
 }
 
