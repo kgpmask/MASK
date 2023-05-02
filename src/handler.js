aff1db6113b96e9caa9b0011598a7defec0200a0
Vidunram A R <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-02 12:18:09 +0530

    Release 3.2.0
    * Insta Artworks (#118)
    
    Co-authored-by: PartMan <47669599+PartMan7@users.noreply.github.com>
    Co-authored-by: Goose <64352864+Goose-Of-War@users.noreply.github.com>
    
    * Increment version
    
    * git hook issue
    
    * logic...
    
    * is this it?
    
    * Revert "is this it?"
    
    This reverts commit 8043dc8e643a3b9fc51e69a6065207385874ae32.
    
    * is this it?
    
    * April Newsletter (#126)
    
    * Insta artwork - with metadata (#127)
    
    
    ---------
    
    Co-authored-by: PartMan <47669599+PartMan7@users.noreply.github.com>
    Co-authored-by: Ankan <ankansahadgp@gmail.com>
    
    * this is jai from ankan's laptop yes
    
    * i think this works
    
    * Final touches
    
    * Add tests for error handling
    
    * Version increment
    
    ---------
    
    Co-authored-by: Ankan <93087057+ItsAnkan@users.noreply.github.com>
    Co-authored-by: PartMan <47669599+PartMan7@users.noreply.github.com>
    Co-authored-by: PartMan <mane.parth@gmail.com>
    Co-authored-by: Ankan <ankansahadgp@gmail.com>
    Co-authored-by: SachdevJai <jaisachdev@kgpian.iitkgp.ac.in>

--- src/handler.js
+++ src/handler.js
@@ -721,25 +721,36 @@ function handler (app, nunjEnv) {
 		}
 		const branch = process.env.WEBHOOK_BRANCH;
 		if (!branch) return res.send('No branch configured for webhooks');
-		if (branch !== 'dev') return res.send('Automatic webhook updates are only enabled on the dev branch');
+		if (branch !== 'dev' && branch !== 'main') {
+			return res.send('Automatic webhook updates are only enabled on dev and main branch');
+		}
 		await Tools.updateCode();
 		res.send('Success!');
 		return process.exit(0);
+	});
+	app.use('/error', async () => {
+		throw new Error('Sensitive error');
+	});
+
+
+	app.use((req, res, next) => {
+		// If propagation hasn't stopped, switch to GET!
+		if (req.method === "POST") {
+			return res.redirect(req.url);
+		}
+		next();
+	});
+	app.use((req, res, next) => {
+		// Catch-all 404
+		res.notFound();
 	});
 
 	app.use((err, req, res, next) => {
 		if (PARAMS.dev) console.error(err.stack);
 		// Make POST errors show only the data, and GET errors show the page with the error message
-		res.status(500).renderFile('404.njk', { message: 'Server error! This may or may not be due to invalid input.' });
-	});
-
-	app.post((req, res) => {
-		// If propagation hasn't stopped, switch to GET!
-		return res.redirect(req.url);
-	});
-	app.use((req, res) => {
-		// Catch-all 404
-		res.notFound();
+		res.status(500);
+		if (req.method === 'GET') res.renderFile('404.njk', { message: 'Server error! This may or may not be due to invalid input.' });
+		else res.send(err.toString());
 	});
 }
 
