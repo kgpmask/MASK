4c53befee65d4625e0e25681cd83e3814c939fdc
SachdevJai <jaisachdev@kgpian.iitkgp.ac.in>
2023-05-16 12:51:04 +0530

    doesn't work

--- routes/govportal.js
+++ routes/govportal.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const { getMembersbyYear } = require('../database/handler');
 const router = express.Router();
 
-router.get(['/', '/:action'], (req, res) => {
+const dbh = PARAMS.mongoless ? {} : require('../database/handler');
+
+router.get(['/', '/:action'], async (req, res) => {
 	// if (!req.loggedIn) return res.redirect('/login');
-	res.renderFile(`govportal/${req.params.action ? req.params.action : 'govportal'}.njk`);
+
+	if (req.params.action === 'member-management') {
+		const teamsData = require('../src/teams.json');
+		const years = Object.keys(teamsData);
+		const currentMembers = await dbh.getMembersbyYear(Number(years[years.length - 1]));
+		console.log(currentMembers[1].teams);
+		res.renderFile(`govportal/${req.params.action ? req.params.action : 'govportal'}.njk`, { currentMembers });
+	} else {
+		res.renderFile(`govportal/${req.params.action ? req.params.action : 'govportal'}.njk`);
+	}
 });
 
 module.exports = router;
