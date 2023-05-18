e5b47ceb0dfa98ba42d1ac1ebc6899c7b533c2a6
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-18 21:20:02 +0530

    @ItsAnkan it works now

--- routes/govportal.js
+++ routes/govportal.js
@@ -4,11 +4,11 @@ const dbh = PARAMS.mongoless ? {} : require('../database/handler');
 
 router.get(['/', '/:action'], (req, res) => {
 	// if (!req.loggedIn) return res.redirect('/login');
-	res.renderFile(`govportal/${req.params.action ? req.params.action : 'govportal'}.njk`);
+	return res.renderFile(`govportal/${req.params.action ? req.params.action : 'govportal'}.njk`);
 });
 
 router.post('/post', async (req, res) => {
-	if (!req.loggedIn) return res.renderFile('/');
+	// if (!req.loggedIn) return res.redirect('/');
 	const data = req.body.data;
 	if (!Object.values(data).some(e => e)) {
 		return res.send("Empty Data");
