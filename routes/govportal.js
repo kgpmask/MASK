eb48f5b1faf2337319724451c344041952cf3c37
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-18 09:44:13 +0530

    Some edits

--- routes/govportal.js
+++ routes/govportal.js
@@ -8,11 +8,29 @@ router.get(['/', '/:action'], async (req, res) => {
 	// if (!req.loggedIn) return res.redirect('/login');
 
 	if (req.params.action === 'member-management') {
+		const hierarchy = [
+			'Governor',
+			'Team Heads',
+			'Executives',
+			'Research Associate',
+			'Team Sub-Heads',
+			'Associate',
+			'Fresher',
+			'Former Member'
+		];
 		const teamsData = require('../src/teams.json');
 		const years = Object.keys(teamsData);
 		const currentMembers = await dbh.getMembersbyYear(Number(years[years.length - 1]));
-		console.log(currentMembers[1].teams);
-		res.renderFile(`govportal/${req.params.action ? req.params.action : 'govportal'}.njk`, { currentMembers });
+		currentMembers
+			.sort((a, b) => -(hierarchy.indexOf(a.position) < hierarchy.indexOf(b.position)))
+			.forEach(member => {
+				member.teams = member.teams.map(team => team.name);
+			});
+		console.log(currentMembers[currentMembers.length - 3]);
+		res.renderFile(`govportal/member-management.njk`, {
+			currentMembers,
+			teams: Object.values(teamsData[years[years.length - 1]])
+		});
 	} else {
 		res.renderFile(`govportal/${req.params.action ? req.params.action : 'govportal'}.njk`);
 	}
