dd602fccd2173362f8c5dc9446a12141c798e264
SachdevJai <jaisachdev@kgpian.iitkgp.ac.in>
2023-05-19 12:18:44 +0530

    frontend done ig

--- routes/govportal.js
+++ routes/govportal.js
@@ -1,39 +1,37 @@
 const express = require('express');
-const { getMembersbyYear } = require('../database/handler');
 const router = express.Router();
-
 const dbh = PARAMS.mongoless ? {} : require('../database/handler');
 
-router.get(['/', '/:action'], async (req, res) => {
-	// if (!req.loggedIn) return res.redirect('/login');
+router.get('/member-management', async (req, res) => {
+	const hierarchy = [
+		'Governor',
+		'Team Heads',
+		'Executives',
+		'Research Associate',
+		'Team Sub-Heads',
+		'Associate',
+		'Fresher',
+		'Former Member'
+	];
 
-	if (req.params.action === 'member-management') {
-		const hierarchy = [
-			'Governor',
-			'Team Heads',
-			'Executives',
-			'Research Associate',
-			'Team Sub-Heads',
-			'Associate',
-			'Fresher',
-			'Former Member'
-		];
-		const teamsData = require('../src/teams.json');
-		const years = Object.keys(teamsData);
-		const currentMembers = await dbh.getMembersbyYear(Number(years[years.length - 1]));
-		currentMembers
-			.sort((a, b) => -(hierarchy.indexOf(a.position) < hierarchy.indexOf(b.position)))
-			.forEach(member => {
-				member.teams = member.teams.map(team => team.name);
-			});
-		console.log(currentMembers[currentMembers.length - 3]);
-		res.renderFile(`govportal/member-management.njk`, {
-			currentMembers,
-			teams: Object.values(teamsData[years[years.length - 1]])
-		});
-	} else {
-		res.renderFile(`govportal/${req.params.action ? req.params.action : 'govportal'}.njk`);
-	}
+	const teamsData = require('../src/teams.json');
+	const years = Object.keys(teamsData);
+	const currentMembers = await dbh.getMembersbyYear(Number(years[years.length - 1]));
+
+	currentMembers.sort((a, b) => -(hierarchy.indexOf(a.position) < hierarchy.indexOf(b.position))).forEach(member => {
+		member.teams = member.teams.map(team => team.name);
+	});
+
+	res.renderFile('/govportal/member-management.njk', { currentMembers, teams: Object.values(teamsData[years[years.length - 1]]) });
+});
+
+router.get('/', (req, res) => {
+	// if (!req.loggedIn) return res.redirect('/login');
+	res.renderFile('/govportal/govportal.njk');
+});
+
+router.get('/image-upload', (req, res) => {
+	res.renderFile('/govportal/image-upload.njk');
 });
 
 module.exports = router;
