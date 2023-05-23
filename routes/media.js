1d44e60457bac15b6cef66d4dbe5aa2c609f6e69
Symbiot01 <sahil0111patel@gmail.com>
2023-05-24 02:17:31 +0530

    Test Error fix

--- routes/media.js
+++ routes/media.js
@@ -7,13 +7,15 @@ const path = require('path');
 const dbh = PARAMS.mongoless ? {} : require('../database/handler');
 
 router.get('/art', async (req, res) => {
-	const sample = JSON.parse(fs.readFileSync(path.join(__dirname, "../src/samples/posts.json"), 'utf8')).filter(post => post.type === 'art' && post.hype);
+	const sample = JSON.parse(fs.readFileSync(path.join(__dirname, "../src/samples/posts.json"), 'utf8'))
+		.filter(post => post.type === 'art' && post.hype);
 	const art = PARAMS.mongoless ? sample : await dbh.getPosts('art');
 	return res.renderFile('art.njk', { art });
 });
 
 router.get('/videos', async (req, res) => {
-	const sample = JSON.parse(fs.readFileSync(path.join(__dirname, "../src/samples/posts.json"), 'utf8')).filter(post => post.type === 'youtube' && post.hype);
+	const sample = JSON.parse(fs.readFileSync(path.join(__dirname, "../src/samples/posts.json"), 'utf8'))
+		.filter(post => post.type === 'youtube' && post.hype);
 	const vids = PARAMS.mongoless ? sample : await dbh.getPosts("youtube");
 	vids.forEach((vid) => {
 		vid.embed = `https://www.youtube.com/embed/${vid.link.split('?v=')[1]}?playsinline=1`;
