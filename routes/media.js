ae46519533f91f514a686dc5ab34a82f30d0116a
Symbiot01 <114978589+Symbiot01@users.noreply.github.com>
2023-05-24 13:26:39 +0530

    Migrate (#145)

--- routes/media.js
+++ routes/media.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+const path = require('path');
+
 const dbh = PARAMS.mongoless ? {} : require('../database/handler');
+const sample = require('../src/samples/posts');
 
 router.get('/art', async (req, res) => {
-	const sample = [
-		{
-			name: 'Art - Tanjiro Kamado',
-			link: '0025.webp',
-			type: 'art',
-			attr: ['Sanjeev Raj Ganji'],
-			date: new Date(1630261800000),
-			hype: true
-		}
-	];
-	const art = PARAMS.mongoless ? sample : await dbh.getPosts('art');
+	const art = PARAMS.mongoless ? sample.filter(post => post.type === 'art') : await dbh.getPosts('art');
 	return res.renderFile('art.njk', { art });
 });
 
 router.get('/videos', async (req, res) => {
-	const sample = [{ name: 'How to get into MASK', link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', type: 'youtube' }];
-	const vids = PARAMS.mongoless ? sample : await dbh.getPosts('youtube');
-	vids.forEach(vid => vid.embed = `https://www.youtube.com/embed/${vid.link.split('?v=')[1]}?playsinline=1`);
+	const vids = PARAMS.mongoless ? sample.filter(post => post.type === 'youtube') : await dbh.getPosts("youtube");
+	vids.forEach((vid) => {
+		vid.embed = `https://www.youtube.com/embed/${vid.link.split('?v=')[1]}?playsinline=1`;
+	});
 	return res.renderFile('videos.njk', { vids });
 });
 
