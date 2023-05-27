a6c6cfb3840a92b1d88be99f4817591f79af3fb1
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-27 12:58:55 +0530

    Changing --restart always to update

--- docker/docker.sh
+++ docker/docker.sh
@@ -5,6 +5,7 @@ if [ "$1" == "dev" ] ; then
 	fi
 	command docker container rm mask_dev
 	command docker run --name mask_dev --restart always -d -p 6971:6969 mask_dev_image &&
+	command docker update --restart always mask_dev
 	command echo "Dev docker is running!"
 elif [ "$1" == "prod" ] ; then
 	if ! command docker build -t mask_image -f ./docker/Dockerfile .
@@ -13,6 +14,7 @@ elif [ "$1" == "prod" ] ; then
 	fi
 	command docker container rm mask
 	command docker run --name mask --restart always -d -p 6969:6969 mask_image &&
+	command docker update --restart always mask
 	command echo "Prod docker is running!"
 else
 	command echo "No environment specified."
