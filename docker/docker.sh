b75ac05f5ca143fbd37f56b921e00373b280002d
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-27 14:05:04 +0530

    Now run both dockers using same line of code

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,4 +1,5 @@
-if [ "$1" == "dev" ] ; then
+function deploy_dev () {
+	command echo "Building dev docker: " 
 	if ! command docker build -t mask_dev_image -f ./docker/Dockerfile-dev .
 	then
 		exit 1
@@ -8,8 +9,10 @@ if [ "$1" == "dev" ] ; then
 	command docker run --name mask_dev --restart always -d -p 6971:6969 mask_dev_image &&
 	command docker update --restart always mask_dev
 	command echo "Dev docker is running!"
-elif [ "$1" == "prod" ] ; then
-	if ! command docker build -t mask_image -f ./docker/Dockerfile .
+}
+
+function deploy_prod () {
+if ! command docker build -t mask_image -f ./docker/Dockerfile .
 	then
 		exit 1
 	fi
@@ -18,6 +21,15 @@ elif [ "$1" == "prod" ] ; then
 	command docker run --name mask --restart always -d -p 6969:6969 mask_image &&
 	command docker update --restart always mask
 	command echo "Prod docker is running!"
+}
+
+if [ "$1" == "all" ] ; then 
+	deploy_dev
+	deploy_prod
+elif [ "$1" == "dev" ] ; then
+	deploy_dev
+elif [ "$1" == "prod" ] ; then
+	deploy_prod
 else
 	command echo "No environment specified."
 fi
