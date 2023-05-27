f8ef2bcb7d13ba2cbca67e5ca870525ad998c36e
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-27 10:11:00 +0530

    fs not needed there

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const fs = require('fs');
 const path = require('path');
 
 const dbh = PARAMS.mongoless ? {} : require('../database/handler');
