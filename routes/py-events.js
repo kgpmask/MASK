4c222d89b281e475b9fdb012f051ee7fa83905c0
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-05-30 14:32:34 +0530

    Seekers' Quest has been solved

--- routes/py-events.js
+++ routes/py-events.js
@@ -5,16 +5,20 @@ router.get('/', (req, res) => {
 	return res.renderFile("event-articles/py_events.njk");
 });
 
+router.get('/qatq', (req, res) => {
+	return res.renderFile("event-articles/qatq.njk");
+});
+
+router.get('/seekers-quest', (req, res) => {
+	return res.renderFile("event-articles/seekers-quest.njk");
+});
+
 router.get('/cosplay23', (req, res) => {
 	return res.renderFile("event-articles/cosplay23.njk");
 });
 
 router.get('/intrasoc', (req, res) => {
 	return res.renderFile("event-articles/intrasoc.njk");
-});
-
-router.get('/qatq', (req, res) => {
-	return res.renderFile("event-articles/qatq.njk");
 });
 
 router.get('/suzume', (req, res) => {
