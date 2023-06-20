33106a7093e289024b2e31e03562153dfbaef9f3
SachdevJai <118045698+SachdevJai@users.noreply.github.com>
2023-06-20 15:50:22 +0530

    Update govportal.js

--- routes/govportal.js
+++ routes/govportal.js
@@ -1,5 +1,4 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 
 const dbh = PARAMS.mongoless ? {} : require("../database/handler");
 
