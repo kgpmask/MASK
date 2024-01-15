217828225840858e0fafeb1ed836c256c9b63d75
destryptor <sharanya.chakraborty@kgpian.iitkgp.ac.in>
2024-01-15 22:29:53 +0530

    Probably final fixes

--- database/handler.js
+++ database/handler.js
@@ -7,7 +7,6 @@ const Poll = require('./schemas/Poll');
 const Post = require('./schemas/Post');
 const Submission = require('./schemas/Submission');
 const NewsletterCount = require('./schemas/NewsletterCount');
-const newsletter = require('../routes/newsletter');
 
 // Handle newly registered user or normal login
 async function createNewUser (profile) {
@@ -353,7 +352,7 @@ async function updateNewsletterCount (target) {
 	const newsletterCount = await NewsletterCount.findById(target) || new NewsletterCount({
 		_id: target,
 		count: 0
-	})
+	});
 	newsletterCount.count = newsletterCount.count + 1;
 	return await newsletterCount.save();
 }
--- routes/newsletter.js
+++ routes/newsletter.js
@@ -34,7 +34,7 @@ router.post('/newsletter-management', async (req, res) => {
 	const { newsletterId } = req.body;
 
 	try {
-		dbh.updateNewsletterCount(newsletterId);
+		await dbh.updateNewsletterCount(newsletterId);
 		return res.status(200).json({ success: true, message: 'View count updated successfully' });
 	} catch (error) {
 		console.error('Error updating view count:', error);
