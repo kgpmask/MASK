82086fb06e74dca7e3734a98af3115d9d5d73a69
destryptor <sharanya.chakraborty@kgpian.iitkgp.ac.in>
2024-10-05 12:28:03 +0530

    Webhook additions, tho i think it needs changes

--- src/hooks.js
+++ src/hooks.js
@@ -19,33 +19,47 @@ async function deployErrorHook (env, commit, err) {
 	return 'Success';
 }
 
-async function submissionHook (data) {
+async function submissionHook(data) {
 	const webhookLink = hooks.submission;
 	const dataTypes = {
-		'dig-art': 'Digital Art',
-		'trd-art': 'Traditional Art',
-		'amv-vid': 'AMV Video',
-		'ani-vid': 'Animation Video',
-		'ins-mus': 'Instrumental Music',
-		'voc-mus': 'Vocal Music'
+	  'dig-art': 'Digital Art',
+	  'trd-art': 'Traditional Art',
+	  'amv-vid': 'AMV Video',
+	  'ani-vid': 'Animation Video',
+	  'ins-mus': 'Instrumental Music',
+	  'voc-mus': 'Vocal Music'
 	};
+	
+	const timestamp = new Date(data.date).toLocaleString('en-GB', {
+	  year: 'numeric',
+	  month: '2-digit',
+	  day: '2-digit',
+	  hour: '2-digit',
+	  minute: '2-digit',
+	  second: '2-digit',
+	});
+  
 	const webhookObject = {
-		embeds: [
-			{
-				title: `Submission: ${ dataTypes[data.type] }`,
-				fields: [
-					{ name: 'Email', value: `${ data.email }` },
-					{ name: 'Name', value: `${ data.name }` },
-					{ name: 'Link', value: `[Submission Link](${ data.link })` },
-					{ name: 'Member of KGP', value: `${ data.member ? 'Yes' : 'No' }`, inline: true },
-					{ name: 'Proof', value: `${ data.proof ?? 'Not provided' }`, inline: true }
-				]
-			}
-		]
+	  embeds: [
+		{
+		  title: `Submission: ${dataTypes[data.type]}`,
+		  fields: [
+			{ name: 'Email', value: `${data.email}` },
+			{ name: 'Name', value: `${data.name}` },
+			{ name: 'Link', value: `[Submission Link](${data.link})` },
+			{ name: 'Member of KGP', value: `${data.member ? 'Yes' : 'No'}`, inline: true },
+			{ name: 'Proof', value: `${data.proof ?? 'Not provided'}`, inline: true },
+			{ name: 'Social Media Handle', value: `${data.social ?? 'Not provided'}`, inline: true },
+			{ name: 'Timestamp', value: `${timestamp}` }
+		  ]
+		}
+	  ]
 	};
+  
 	await axios.post(webhookLink, webhookObject);
 	return 'Success';
-}
+  }
+  
 
 
 module.exports = {
